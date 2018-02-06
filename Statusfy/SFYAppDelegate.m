8abf9ee32ce9889c222cfc463dcd80377171e68c
fxj9a <adrian.sprucer@gmail.com>
2018-02-06 10:00:29 +0100

    new format: <playicon> <title> by <artist>
    why did I want it the other way around?
    i like it like this  now, i guess.

--- Statusfy/SFYAppDelegate.m
+++ Statusfy/SFYAppDelegate.m
@@ -54,7 +54,7 @@ static NSString * const SFYPlayerDockIconPreferenceKey = @"YES";
     NSString *artistName = [[self executeAppleScript:@"get artist of current track"] stringValue];
     
     if (trackName && artistName) {
-        NSString *titleText = [NSString stringWithFormat:@"%@ - %@", artistName, trackName];
+        NSString *titleText = [NSString stringWithFormat:@"%@ by %@", trackName, artistName];
         
         if ([self getPlayerStateVisibility]) {
             NSString *playerState = [self determinePlayerStateText];
